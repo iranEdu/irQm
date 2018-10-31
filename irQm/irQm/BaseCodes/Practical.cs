9af6a81bd961965501f8f2568b512342be1fddf0
samira taghilooniya <s.taghilonia@gmail.com>
2018-11-01 00:10:49 +0330

    t1

--- irQm/irQm/BaseCodes/Practical.cs
+++ irQm/irQm/BaseCodes/Practical.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +8,11 @@ namespace irQm.BaseCodes
 {
     class Practical : IQuestion
     {
-        [MaxLength(50)]
-        public string Id { get; set; }
         public string Face { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public float Score { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public float gainedScore { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public List<string> checkList { get; set; } = new List<string>();
         public List<string> Tags { get; set; } = new List<string>();
         public Lesson Lesson { get; set; }
-        //test
     }
 }
