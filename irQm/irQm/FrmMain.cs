a90cb231142edbc60efb195f41d180e397ce52b0
s.taghilonia <s.taghilonia@gmail.com>
2018-11-25 14:58:08 +0330

    design of first and second page

---
+++ irQm/irQm/FrmMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace irQm
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        internal new static void Show()
+        {
+            throw new NotImplementedException();
+        }
+
+       
+        
+    }
+}
