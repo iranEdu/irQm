61e7fb63d785a81309c1048a3dd15af8182e860b
hsafavi <hossein.safavi@gmail.com>
2019-02-09 16:32:28 +0330

    fi

---
+++ irQm/irQm/Forms/FrmItems.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace irQm.Forms
+{
+    partial class FrmItems
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(549, 341);
+            this.flowLayoutPanel1.TabIndex = 138;
+            this.flowLayoutPanel1.WrapContents = false;
+            // 
+            // SelectChoiceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 341);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "SelectChoiceForm";
+            this.Text = "SelectChoiceForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
---
+++ irQm/irQm/Forms/FrmItems.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace irQm.Forms
+{
+    public partial class FrmItems : Form
+    {
+        
+        public FrmItems(List<string> selection)
+
+        {
+            InitializeComponent();
+            
+            //foreach (string s in selection )
+            for (int i = 0; i < selection.Count; i++)
+            {
+                Label label1 = new Label();
+                label1.AutoSize = true;
+                label1.Text = selection[i];
+                flowLayoutPanel1.Controls.Add(label1);
+            }
+        }
+    }
+}
+
