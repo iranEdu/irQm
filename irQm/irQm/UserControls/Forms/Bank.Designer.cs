fded863b6f8a3d56aa985ae3699275e3ac110262
hsafavi <hossein.safavi@gmail.com>
2019-02-24 01:14:22 +0330

    q bank

--- irQm/irQm/UserControls/Forms/Bank.Designer.cs
+++ irQm/irQm/UserControls/Forms/Bank.Designer.cs
@@ -30,17 +30,23 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.multiTabPage = new System.Windows.Forms.TabPage();
-            this.tfTabPage = new System.Windows.Forms.TabPage();
             this.flpMultiOptionsQuestions = new System.Windows.Forms.FlowLayoutPanel();
-            this.puzzleTabPage = new System.Windows.Forms.TabPage();
-            this.shortTabPage = new System.Windows.Forms.TabPage();
-            this.longTabPage = new System.Windows.Forms.TabPage();
-            this.practicalTabPage = new System.Windows.Forms.TabPage();
+            this.tfTabPage = new System.Windows.Forms.TabPage();
             this.flpTFQuestions = new System.Windows.Forms.FlowLayoutPanel();
+            this.puzzleTabPage = new System.Windows.Forms.TabPage();
             this.flpPuzzleQuestions = new System.Windows.Forms.FlowLayoutPanel();
+            this.shortTabPage = new System.Windows.Forms.TabPage();
             this.flpShortQuestions = new System.Windows.Forms.FlowLayoutPanel();
+            this.longTabPage = new System.Windows.Forms.TabPage();
             this.flpLongQuestions = new System.Windows.Forms.FlowLayoutPanel();
+            this.practicalTabPage = new System.Windows.Forms.TabPage();
             this.flpPracticalQuestions = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tagsBox1 = new irQm.TagsBox();
+            this.txtSearchExpr = new irQm.UserControls.Utilities.AutoDirectionTextbox();
             this.tabControl1.SuspendLayout();
             this.multiTabPage.SuspendLayout();
             this.tfTabPage.SuspendLayout();
@@ -48,23 +54,26 @@
             this.shortTabPage.SuspendLayout();
             this.longTabPage.SuspendLayout();
             this.practicalTabPage.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.multiTabPage);
             this.tabControl1.Controls.Add(this.tfTabPage);
             this.tabControl1.Controls.Add(this.puzzleTabPage);
             this.tabControl1.Controls.Add(this.shortTabPage);
             this.tabControl1.Controls.Add(this.longTabPage);
             this.tabControl1.Controls.Add(this.practicalTabPage);
-            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Location = new System.Drawing.Point(0, 127);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.tabControl1.RightToLeftLayout = true;
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(632, 482);
+            this.tabControl1.Size = new System.Drawing.Size(632, 355);
             this.tabControl1.TabIndex = 0;
             this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
             // 
@@ -74,21 +83,10 @@
             this.multiTabPage.Location = new System.Drawing.Point(4, 22);
             this.multiTabPage.Name = "multiTabPage";
             this.multiTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.multiTabPage.Size = new System.Drawing.Size(624, 456);
+            this.multiTabPage.Size = new System.Drawing.Size(624, 329);
             this.multiTabPage.TabIndex = 0;
             this.multiTabPage.Text = "چند گزینه‌ای";
             this.multiTabPage.UseVisualStyleBackColor = true;
-            // 
-            // tfTabPage
-            // 
-            this.tfTabPage.Controls.Add(this.flpTFQuestions);
-            this.tfTabPage.Location = new System.Drawing.Point(4, 22);
-            this.tfTabPage.Name = "tfTabPage";
-            this.tfTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.tfTabPage.Size = new System.Drawing.Size(624, 456);
-            this.tfTabPage.TabIndex = 1;
-            this.tfTabPage.Text = "درست/ نادرست";
-            this.tfTabPage.UseVisualStyleBackColor = true;
             // 
             // flpMultiOptionsQuestions
             // 
@@ -100,53 +98,20 @@
             this.flpMultiOptionsQuestions.Name = "flpMultiOptionsQuestions";
             this.flpMultiOptionsQuestions.Padding = new System.Windows.Forms.Padding(5);
             this.flpMultiOptionsQuestions.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.flpMultiOptionsQuestions.Size = new System.Drawing.Size(618, 450);
+            this.flpMultiOptionsQuestions.Size = new System.Drawing.Size(618, 323);
             this.flpMultiOptionsQuestions.TabIndex = 144;
             this.flpMultiOptionsQuestions.WrapContents = false;
             // 
-            // puzzleTabPage
+            // tfTabPage
             // 
-            this.puzzleTabPage.Controls.Add(this.flpPuzzleQuestions);
-            this.puzzleTabPage.Location = new System.Drawing.Point(4, 22);
-            this.puzzleTabPage.Name = "puzzleTabPage";
-            this.puzzleTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.puzzleTabPage.Size = new System.Drawing.Size(624, 456);
-            this.puzzleTabPage.TabIndex = 2;
-            this.puzzleTabPage.Text = "جورکردنی";
-            this.puzzleTabPage.UseVisualStyleBackColor = true;
-            // 
-            // shortTabPage
-            // 
-            this.shortTabPage.Controls.Add(this.flpShortQuestions);
-            this.shortTabPage.Location = new System.Drawing.Point(4, 22);
-            this.shortTabPage.Name = "shortTabPage";
-            this.shortTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.shortTabPage.Size = new System.Drawing.Size(624, 456);
-            this.shortTabPage.TabIndex = 3;
-            this.shortTabPage.Text = "کوتاه پاسخ";
-            this.shortTabPage.UseVisualStyleBackColor = true;
-            // 
-            // longTabPage
-            // 
-            this.longTabPage.Controls.Add(this.flpLongQuestions);
-            this.longTabPage.Location = new System.Drawing.Point(4, 22);
-            this.longTabPage.Name = "longTabPage";
-            this.longTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.longTabPage.Size = new System.Drawing.Size(624, 456);
-            this.longTabPage.TabIndex = 4;
-            this.longTabPage.Text = "بلند پاسخ";
-            this.longTabPage.UseVisualStyleBackColor = true;
-            // 
-            // practicalTabPage
-            // 
-            this.practicalTabPage.Controls.Add(this.flpPracticalQuestions);
-            this.practicalTabPage.Location = new System.Drawing.Point(4, 22);
-            this.practicalTabPage.Name = "practicalTabPage";
-            this.practicalTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.practicalTabPage.Size = new System.Drawing.Size(624, 456);
-            this.practicalTabPage.TabIndex = 5;
-            this.practicalTabPage.Text = "عملی";
-            this.practicalTabPage.UseVisualStyleBackColor = true;
+            this.tfTabPage.Controls.Add(this.flpTFQuestions);
+            this.tfTabPage.Location = new System.Drawing.Point(4, 22);
+            this.tfTabPage.Name = "tfTabPage";
+            this.tfTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.tfTabPage.Size = new System.Drawing.Size(624, 329);
+            this.tfTabPage.TabIndex = 1;
+            this.tfTabPage.Text = "درست/ نادرست";
+            this.tfTabPage.UseVisualStyleBackColor = true;
             // 
             // flpTFQuestions
             // 
@@ -158,9 +123,20 @@
             this.flpTFQuestions.Name = "flpTFQuestions";
             this.flpTFQuestions.Padding = new System.Windows.Forms.Padding(5);
             this.flpTFQuestions.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.flpTFQuestions.Size = new System.Drawing.Size(618, 450);
+            this.flpTFQuestions.Size = new System.Drawing.Size(618, 323);
             this.flpTFQuestions.TabIndex = 145;
             this.flpTFQuestions.WrapContents = false;
+            // 
+            // puzzleTabPage
+            // 
+            this.puzzleTabPage.Controls.Add(this.flpPuzzleQuestions);
+            this.puzzleTabPage.Location = new System.Drawing.Point(4, 22);
+            this.puzzleTabPage.Name = "puzzleTabPage";
+            this.puzzleTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.puzzleTabPage.Size = new System.Drawing.Size(624, 329);
+            this.puzzleTabPage.TabIndex = 2;
+            this.puzzleTabPage.Text = "جورکردنی";
+            this.puzzleTabPage.UseVisualStyleBackColor = true;
             // 
             // flpPuzzleQuestions
             // 
@@ -172,9 +148,20 @@
             this.flpPuzzleQuestions.Name = "flpPuzzleQuestions";
             this.flpPuzzleQuestions.Padding = new System.Windows.Forms.Padding(5);
             this.flpPuzzleQuestions.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.flpPuzzleQuestions.Size = new System.Drawing.Size(618, 450);
+            this.flpPuzzleQuestions.Size = new System.Drawing.Size(618, 323);
             this.flpPuzzleQuestions.TabIndex = 145;
             this.flpPuzzleQuestions.WrapContents = false;
+            // 
+            // shortTabPage
+            // 
+            this.shortTabPage.Controls.Add(this.flpShortQuestions);
+            this.shortTabPage.Location = new System.Drawing.Point(4, 22);
+            this.shortTabPage.Name = "shortTabPage";
+            this.shortTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.shortTabPage.Size = new System.Drawing.Size(624, 329);
+            this.shortTabPage.TabIndex = 3;
+            this.shortTabPage.Text = "کوتاه پاسخ";
+            this.shortTabPage.UseVisualStyleBackColor = true;
             // 
             // flpShortQuestions
             // 
@@ -186,9 +173,20 @@
             this.flpShortQuestions.Name = "flpShortQuestions";
             this.flpShortQuestions.Padding = new System.Windows.Forms.Padding(5);
             this.flpShortQuestions.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.flpShortQuestions.Size = new System.Drawing.Size(618, 450);
+            this.flpShortQuestions.Size = new System.Drawing.Size(618, 323);
             this.flpShortQuestions.TabIndex = 145;
             this.flpShortQuestions.WrapContents = false;
+            // 
+            // longTabPage
+            // 
+            this.longTabPage.Controls.Add(this.flpLongQuestions);
+            this.longTabPage.Location = new System.Drawing.Point(4, 22);
+            this.longTabPage.Name = "longTabPage";
+            this.longTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.longTabPage.Size = new System.Drawing.Size(624, 329);
+            this.longTabPage.TabIndex = 4;
+            this.longTabPage.Text = "بلند پاسخ";
+            this.longTabPage.UseVisualStyleBackColor = true;
             // 
             // flpLongQuestions
             // 
@@ -200,9 +198,20 @@
             this.flpLongQuestions.Name = "flpLongQuestions";
             this.flpLongQuestions.Padding = new System.Windows.Forms.Padding(5);
             this.flpLongQuestions.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.flpLongQuestions.Size = new System.Drawing.Size(618, 450);
+            this.flpLongQuestions.Size = new System.Drawing.Size(618, 323);
             this.flpLongQuestions.TabIndex = 145;
             this.flpLongQuestions.WrapContents = false;
+            // 
+            // practicalTabPage
+            // 
+            this.practicalTabPage.Controls.Add(this.flpPracticalQuestions);
+            this.practicalTabPage.Location = new System.Drawing.Point(4, 22);
+            this.practicalTabPage.Name = "practicalTabPage";
+            this.practicalTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.practicalTabPage.Size = new System.Drawing.Size(624, 329);
+            this.practicalTabPage.TabIndex = 5;
+            this.practicalTabPage.Text = "عملی";
+            this.practicalTabPage.UseVisualStyleBackColor = true;
             // 
             // flpPracticalQuestions
             // 
@@ -214,12 +223,79 @@
             this.flpPracticalQuestions.Name = "flpPracticalQuestions";
             this.flpPracticalQuestions.Padding = new System.Windows.Forms.Padding(5);
             this.flpPracticalQuestions.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.flpPracticalQuestions.Size = new System.Drawing.Size(618, 450);
+            this.flpPracticalQuestions.Size = new System.Drawing.Size(618, 323);
             this.flpPracticalQuestions.TabIndex = 145;
             this.flpPracticalQuestions.WrapContents = false;
             // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.tagsBox1);
+            this.panel1.Controls.Add(this.txtSearchExpr);
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(41, 14);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(552, 91);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSearch.Location = new System.Drawing.Point(76, 64);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "جستجو";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(474, 6);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "عبارت جستجو:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(505, 32);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "برچسب:";
+            // 
+            // tagsBox1
+            // 
+            this.tagsBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tagsBox1.Location = new System.Drawing.Point(221, 29);
+            this.tagsBox1.Name = "tagsBox1";
+            this.tagsBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.tagsBox1.Size = new System.Drawing.Size(247, 20);
+            this.tagsBox1.TabIndex = 4;
+            // 
+            // txtSearchExpr
+            // 
+            this.txtSearchExpr.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSearchExpr.Location = new System.Drawing.Point(344, 3);
+            this.txtSearchExpr.Name = "txtSearchExpr";
+            this.txtSearchExpr.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtSearchExpr.Size = new System.Drawing.Size(124, 20);
+            this.txtSearchExpr.TabIndex = 3;
+            // 
             // Bank
             // 
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.tabControl1);
             this.Name = "Bank";
             this.Size = new System.Drawing.Size(632, 482);
@@ -237,6 +313,8 @@
             this.longTabPage.PerformLayout();
             this.practicalTabPage.ResumeLayout(false);
             this.practicalTabPage.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -256,5 +334,11 @@
         private System.Windows.Forms.FlowLayoutPanel flpLongQuestions;
         private System.Windows.Forms.TabPage practicalTabPage;
         private System.Windows.Forms.FlowLayoutPanel flpPracticalQuestions;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private TagsBox tagsBox1;
+        private Utilities.AutoDirectionTextbox txtSearchExpr;
     }
 }
--- irQm/irQm/UserControls/Forms/Bank.cs
+++ irQm/irQm/UserControls/Forms/Bank.cs
@@ -11,6 +11,9 @@ namespace irQm.UserControls.Forms
 {
     public partial class Bank : UserControl
     { List<UCQuestionListItem> list=new List<UCQuestionListItem>();
+        RichTextBox rtb = new RichTextBox();
+        private string searchExpr;
+
         public Bank()
         {
             InitializeComponent();
@@ -56,40 +59,59 @@ namespace irQm.UserControls.Forms
 
         private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            search(searchExpr);
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            searchExpr = txtSearchExpr.Text;
+            search(searchExpr);
+        }
+
+        void search(string expr)
+        {
+            var tags = tagsBox1.Tags;
             using (var db = new irQmDbContext())
             {
-                IQuestion[] questions=null;
+                IQuestion[] questions = null;
                 FlowLayoutPanel target = null;
+                
                 switch (tabControl1.SelectedIndex)
                 {
                     case 0:
-                        questions =  db.MultiChoicesQuestions.ToArray();
+                       
+                            questions = db.MultiChoicesQuestions.Where(q => tags.All(tg=>db.TagInMultichoices.Any(t=> t.QuestionId == q.Id&& t.Tag.Value==tg)) && richContains(q.Face,expr)).ToArray();
                         target = flpMultiOptionsQuestions;
                         break;
                     case 1:
-                        questions = db.TFQuestions.ToArray();
+                        
+                            questions = db.TFQuestions.Where(q => tags.All(tg => db.TagInTfQuestion.Any(t => t.QuestionId == q.Id && t.Tag.Value == tg)) && richContains(q.Face, expr)).ToArray();
                         target = flpTFQuestions;
                         break;
                     case 2:
-                        questions = db.PuzzleQuestions.ToArray();
+                       
+                            questions = db.PuzzleQuestions.Where(q => tags.All(tg => db.TagInPuzzle.Any(t => t.QuestionId == q.Id && t.Tag.Value == tg)) && q.Face.Contains(expr)).ToArray();
                         target = flpPuzzleQuestions;
                         break;
                     case 3:
-                        questions = db.ShortAnswerQustions.ToArray();
+                       
+                            questions = db.ShortAnswerQustions.Where(q => tags.All(tg => db.TagInShortAnswer.Any(t => t.QuestionId == q.Id && t.Tag.Value == tg)) && richContains(q.Face, expr)).ToArray();
                         target = flpShortQuestions;
                         break;
                     case 4:
-                        questions = db.LongAnswerQuestions.ToArray();
+                       
+                            questions = db.LongAnswerQuestions.Where(q => tags.All(tg => db.TagInLongAnswer.Any(t => t.QuestionId == q.Id && t.Tag.Value == tg)) && richContains(q.Face, expr)).ToArray();
                         target = flpLongQuestions;
                         break;
                     case 5:
-                        questions = db.PracticalQuestions.ToArray();
+                       
+                            questions = db.PracticalQuestions.Where(q => tags.All(tg => db.TagInPractical.Any(t => t.QuestionId == q.Id && t.Tag.Value == tg)) && richContains(q.Face, expr)).ToArray();
                         target = flpPracticalQuestions;
                         break;
 
                 }
 
-               
+
                 int i = 1;
                 target.Controls.Clear();
                 foreach (var q in questions.OrderBy(q => q.Face))
@@ -105,7 +127,15 @@ namespace irQm.UserControls.Forms
                 }
 
             }
-            
+
+        }
+        bool richContains(string rich,string expr)
+        {
+            if (string.IsNullOrWhiteSpace(expr))
+               return true;
+       
+            rtb.Rtf = rich;
+            return rtb.Text.Contains(expr);
         }
     }
 }
